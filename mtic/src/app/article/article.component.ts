d6dbe62653ae72eed3afde22bdd84edb1841eaa7
Marcel Timmerman <m.timmerman@asito.nl>
2024-01-17 12:18:32 +0100

    Modify content, add dynamic keywords

--- mtic/src/app/article/article.component.ts
+++ mtic/src/app/article/article.component.ts
@@ -5,6 +5,7 @@ import * as selectors from '../store/selectors';
 import { filter, withLatestFrom } from 'rxjs/operators';
 import { Article } from '../store/models/article.model';
 import { Router } from '@angular/router';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-article',
@@ -22,12 +23,21 @@ export class ArticleComponent extends BaseComponent implements OnInit {
   previousArticleButtonText = 'Vorige';
   nextArticleButtonText = 'Volgende';
 
-  constructor(private store: Store, private router: Router) {
+  originalKeywords: string = '';
+
+  constructor(private store: Store, private router: Router, private meta: Meta) {
     super();
+    this.originalKeywords = meta.getTag("name='keywords'")?.content || '';
   }
 
   ngOnInit(): void {
-    this.subscribe(this.article$);
+    this.subscribe(
+      this.article$.pipe(filter(Boolean)),
+      article => {
+        if (article.tags) {
+          this.meta.updateTag({name: 'keywords', content: [ this.originalKeywords, ...article.tags].join(', ')});
+        }
+      });
     this.subscribe(this.previousArticle$);
     this.subscribe(this.nextArticle$);
     this.subscribe(
@@ -42,6 +52,14 @@ export class ArticleComponent extends BaseComponent implements OnInit {
         this.nextArticleButtonText = article.settings.nextButtonText || this.previousArticleButtonText;
       }
     });
+  }
+
+  override ngOnDestroy(): void {
+    super.ngOnDestroy();
+
+    if (this.originalKeywords?.length) {
+      this.meta.updateTag({name: 'keywords', content: this.originalKeywords }, 'name="keywords"');
+    }
   }
 
   to(article: Article): void {
