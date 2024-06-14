4de3ce318c3ae422d0e76b95add90ca2b8674376
Marcel Timmerman <m.timmerman@asito.nl>
2024-06-14 10:53:05 +0200

    #27 Get external markup files

--- mtic/src/app/_partials/article-component/article-component.component.ts
+++ mtic/src/app/_partials/article-component/article-component.component.ts
@@ -1,10 +1,12 @@
 import { AfterContentChecked, Component, Inject, Input, OnInit, ViewChild } from '@angular/core';
-import { Router } from '@angular/router';
 import { Article } from '@app/store/models/article.model';
 import { Store } from '@ngrx/store';
 import * as actions from '../../store/actions';
+import * as selectors from '../../store/selectors';
 import { MarkdownComponent } from 'ngx-markdown';
 import { DOCUMENT } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Observable, filter, map, of, switchMap, take } from 'rxjs';
 
 @Component({
   selector: 'app-article-component',
@@ -12,14 +14,19 @@ import { DOCUMENT } from '@angular/common';
   styleUrls: ['./article-component.component.scss']
 })
 export class ArticleComponentComponent implements OnInit, AfterContentChecked {
-  @ViewChild('content') content: MarkdownComponent | null = null;
+  @ViewChild('contentEl') contentEl: MarkdownComponent | null = null;
   @Input() article: Article | null = null;
   @Input() full = false;
+
+  content$: Observable<string> | null = null;
 
   private readmore = '--readmore--';
   private modifyingContent = false;
 
-  constructor(private router: Router, private store: Store, @Inject(DOCUMENT) private document: any) { }
+  constructor(
+    private httpClient: HttpClient,
+    private store: Store, 
+    @Inject(DOCUMENT) private document: any) { }
 
   ngAfterContentChecked(): void {
     if (this.modifyingContent) {
@@ -28,7 +35,7 @@ export class ArticleComponentComponent implements OnInit, AfterContentChecked {
 
     try
     {
-      const nativeElement = this.content?.element?.nativeElement;
+      const nativeElement = this.contentEl?.element?.nativeElement;
       if (nativeElement) {
         this.modifyingContent = true;
         const links = nativeElement.getElementsByTagName('a');
@@ -50,24 +57,50 @@ export class ArticleComponentComponent implements OnInit, AfterContentChecked {
 
   ngOnInit(): void {
     this.store.dispatch(actions.loadContent());
+
+    let articleId = '';
+    this.content$ = this.full
+      ? this.store.select(selectors.getRouterArticle).pipe(
+          filter(Boolean),
+          filter(a => a.id !== articleId),
+          switchMap(a => {
+            articleId = a.id;
+            if (!a.content) {
+              return of('');
+            }
+
+            switch (a.settings?.externalContent) {
+              case 'markup':
+                return this.httpClient.request('GET', a.content, { responseType: 'text' });
+            }
+        
+            const readmorePos = a.content.indexOf(this.readmore);
+            if (readmorePos === -1) {
+              return of(a.content);
+            }
+    
+            return of(a.content.replace(this.readmore, ''));
+          })
+        )
+      : of(this.article).pipe(
+          filter(Boolean),
+          take(1),
+          map(a => {
+            if (!a.content) {
+              return '';
+            }
+
+            const readmorePos = a.content.indexOf(this.readmore);
+            if (readmorePos === -1) {
+              return a.content;
+            }
+    
+            return a.content.substring(0, readmorePos);
+          }));
   }
 
   getArticleClass(): string {
     return `${this.article?.class} ${this.full ? 'full' : ''}`.trim();
-  }
-
-  getContent(): string {
-    const content = this.article?.content;
-    if (!content) {
-      return '';
-    }
-
-    const readmorePos = content.indexOf(this.readmore);
-    if (readmorePos === -1) {
-      return content;
-    }
-
-    return this.full ? content.replace(this.readmore, '') : content.substring(0, readmorePos);
   }
 
   get hasReadMore(): boolean {
--- mtic/src/app/store/models/articleSettings.model.ts
+++ mtic/src/app/store/models/articleSettings.model.ts
@@ -2,6 +2,7 @@ export class ArticleSettings {
     showTitle = true;
     hidden = false;
     browsable = true;
+    externalContent: null | 'markup' = null;
     categoryIndexes: { [category: string]: number } = {};
     previousButtonText: string | null = null;
     nextButtonText: string | null = null;
@@ -10,6 +11,7 @@ export class ArticleSettings {
         this.showTitle = value?.showTitle === undefined ? true : value.showTitle;
         this.hidden = value?.hidden === undefined ? false : value.hidden;
         this.browsable = value?.browsable === undefined ? true : value.browsable;
+        this.externalContent = value?.externalContent || null;
         this.categoryIndexes = value?.categoryIndexes || {};
         this.previousButtonText = value?.previousButtonText;
         this.nextButtonText = value?.nextButtonText;
